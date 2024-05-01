3f8093de981dface48547882b856f4515388e921
Lindokuhle Mashele <lmashele023b@student.wethinkcode.co.za>
2024-05-01 22:54:51 +0200

    basic enemy added

---
+++ scripts/enemy.gd
@@ -0,0 +1,17 @@
+extends Node2D
+@onready var cast_r  =$ray_cast_r
+@onready var cast_l  =$ray_cast_l
+@onready var enemy_sprite  =$AnimatedSprite2D
+@export var move_speed = 60
+var facing = 1
+
+func _process(delta):
+	if cast_r.is_colliding():
+		print("coll")
+		facing =- 1
+		enemy_sprite.flip_h = true
+	elif cast_l.is_colliding():
+		facing = 1
+		enemy_sprite.flip_h = false
+		
+	position.x += facing *delta*move_speed
